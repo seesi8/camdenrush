b19942237a4fd62ef34af3901607224d9bebb56f
Samuel Liebert <ballbertllc@gmail.com>
2025-01-28 20:21:46 -0600

    favicon + title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './camden.jpg';
-import './App.css';
+import logo from "./camden.jpg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to camdenrush.com
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <title>Camden</title>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Welcome to camdenrush.com</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
